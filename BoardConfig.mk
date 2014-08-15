cd8aad46bfdd12a5be3c18cd03925ae9b2c8a7a1
runforandroid <runforandroid@gmail.com>
2014-08-15 23:18:18 +0800

    add flag for mione_plus

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,3 +84,6 @@ BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 TARGET_RECOVERY_FSTAB := device/xiaomi/mione_plus/ramdisk/fstab.mione
 RECOVERY_FSTAB_VERSION := 2
+
+# flag for xiaomi
+BOARD_USE_XIAOMI_MIONE_HARDWARE := true
